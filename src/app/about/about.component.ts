3a56c089a2ffef5eedca8319c345b0e0f29a4fb7
antony1105 <ntinlok@gmail.com>
2023-11-22 17:40:08 +1100

    ' #

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
   // Define properties and data for the about section
-  name: string = 'John Doe';
+  name: string = 'Mr Ni';
   occupation: string = 'Web Developer';
   bio: string = 'I am a passionate web developer with expertise in Angular, HTML, CSS, and JavaScript. I love creating user-friendly and visually appealing websites.';
 }
